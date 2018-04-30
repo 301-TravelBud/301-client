9ba25ad1fa829a4b8d05b3fd6dff4b6f2ce68520
Andrew Baik <andrewbaik@outlook.com>
2018-04-30 16:23:18 -0700

    Map displays and functaionality in placed

--- models/home-view.js
+++ models/home-view.js
@@ -1,9 +1,27 @@
 'use strict';
 
-//Display map
+//Display map and Marker
+let city = 'code fellows';
+
 function initMap() {
-  var merica = {lat: 82, lng: 40};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: merica
-  });}
+  const map = new google.maps.Map(document.getElementById('map'), {zoom: 6});
+  const geocoder = new google.maps.Geocoder;
+  const travelDetail= city + ' marker comments goes in here';
+  const infowindow = new google.maps.InfoWindow({
+    content: travelDetail
+  });
+
+  geocoder.geocode({'address': city}, function(results, status) {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+
+      const marker = new google.maps.Marker({
+        position: results[0].geometry.location,
+        map: map
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+    }
+  });
+}

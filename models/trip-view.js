2da2c02c5d073974ce8d097f4e724fc76f6e8724
Anthony Green <Anthonydgreen90@gmail.com>
2018-05-02 12:06:15 -0700

    testing rest commands for adding trip changing driver from anthony to mason

--- models/trip-view.js
+++ models/trip-view.js
@@ -1,3 +1,48 @@
-// tripView.initIndexPage = function(ctx, next) {
-//   module.Trip.all.forEach(trip => $('.table-container').append(trip.toHtml()));
-// }
+'use strict';
+
+var app = app || {}
+
+(function (module){
+
+  const tripView = {};
+
+  function show(section) {
+    $('section').not(`#${section}`).hide();
+    $(`#${section}`).show();
+  }
+
+  Trip.initIndexPage = function(ctx) {
+    $('#triplist').empty();
+    $('#trip-view').show();
+    Trip.all.forEach(trip => $('#trip-list').append(trip.toHtml('#trip-table-template')));
+  };
+
+
+  $('#submitTrip').on('submit', addNewTrip);
+
+  function addNewTrip(event) {
+    event.preventDefault();
+    let trip = {
+      country: event.target.country.value,
+      city: event.target.country.value,
+      start_date: event.target.start_date.value,
+      end_date: event.target.end_date.value
+    };
+    $.post(`${ENV.apiUrl}/trips`, trip)
+      .then(app.Trip.fetchAll(tripView.initIndexPage))
+      .catch(console.error);
+    addTripPage();
+  }
+
+  function addTripPage(event) {
+    event.preventDefault();
+    var endOfUrl = window.location.pathname;
+    var n = endOfUrl.lastIndexOf('/');
+    var currID = endOfUrl.substring(n + 1);
+    page(`/trips/${currID}`);
+  }
+
+
+  module.tripView = tripView;
+
+})(app);

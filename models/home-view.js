cecaa543f844c00b8e1a79660b9f029521595c0a
Collin Hintzke <collinhintzke5@gmail.com>
2018-05-02 15:31:40 -0700

    added login features

--- models/home-view.js
+++ models/home-view.js
@@ -24,6 +24,7 @@ var span = document.getElementsByClassName('close')[0];
 // When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = 'block';
+  $('#wrong').css('display', 'none');
 };
 
 // When the user clicks on <span> (x), close the modal
--- scripts/trip.js
+++ scripts/trip.js
@@ -9,10 +9,6 @@ ENV.productionApiUrl = 'https://git.heroku.com/abad-app.git';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 
-page('/'
-  ,(ctx, next) => app.Trip.fetchAll(app.Trip.initIndexPage));
-
-
 
 
 (function (module){
@@ -27,6 +23,49 @@ page('/'
     var template = Handlebars.compile($(htmlID).text());
     return template(this);
   };
+  let currentUser = {};
+  // $('#new-trip-form').on('submit', Trip.userCheck);
+
+  Trip.userCheck = () => {
+    const check = {
+      username: $('#username').val(),
+      password: $('#password').val(),
+      email: $('#userEmail').val()
+    };
+    console.log($('#username').val());
+    Trip.adminCheck(check);
+  };
+
+  Trip.adminCheck = (obj) => {
+    console.log(obj);
+    console.log('in check');
+    $.get(`${ENV.apiUrl}/admin`)
+      .then(results => {
+        console.log(results, 'rip me');
+        for (let i in results) {
+          console.log(results[i]);
+          if (results[i].user_name == obj.username && results[i].password == obj.password) {
+            currentUser = results[i];
+            console.log(currentUser, 'woohoo');
+            console.log('Found a match');
+          } else {
+            $('#wrong').toggle();
+            console.log('Wrong username/pass');
+          }
+        }
+        console.log(results);
+
+      });
+    //   .then(callback)
+    //   .catch(errorCallback);
+  };
+
+
+  Trip.adminView = (callback) => {
+    console.log('hit admin view');
+    callback();
+  };
+
 
   Trip.initIndexPage = function(ctx, next) {
 
@@ -35,7 +74,7 @@ page('/'
   };
 
   Trip.fetchAll = callback => {
-    $.get(`${ENV.apiUrl}/trips`)
+    $.get(`${ENV.apiUrl}`)
       .then(Trip.loadAll)
       .then(callback)
       .catch(console.error);
@@ -53,5 +92,3 @@ page('/'
 
   module.Trip = Trip;
 })(app);
-
-page();

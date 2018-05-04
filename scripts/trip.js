14d7122f8d9b956bd12a3e999087bc273e45bac1
Andrew Baik <andrewbaik@outlook.com>
2018-05-04 09:06:34 -0700

    heroku check

--- scripts/trip.js
+++ scripts/trip.js
@@ -5,7 +5,7 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://git.heroku.com/abad-app.git';
+ENV.productionApiUrl = 'https://abad-app.herokuapp.com/';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 

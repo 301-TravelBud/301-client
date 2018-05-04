3200edff09a31a53c52783e969f9a233739dbd27
Andrew Baik <andrewbaik@outlook.com>
2018-05-04 11:47:17 -0700

    pushing for final

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,15 +1,13 @@
 
 
-page('/', () => app.Trip.fetchAll(app.Trip.initIndexPage()));
+page('/', () => app.Marker.mapMarkers(app.Trip.fetchAll(app.Trip.initIndexPage())));
 
-page('/admin', () => app.Trip.adminView(app.Trip.userCheck(app.Trip.hideModal)));
-// page('/admin', () => app.Trip.userCheck(app.Trip.hideModal));
+page('/admin', () => app.Trip.userCheck(app.Trip.hideModal));
 
 page('/addtrip', () => app.Trip.fetchAll(app.Trip.initTripView()));
 
 page('/aboutus', () => app.Trip.initAboutUsView());
-
-// page('/addtrip', () => app.Trip.fetchAll(app.tripView.initTripView));
+page('/trips', () => app.Trip.fetchAll() );
 
 page();
 page('/createuser', () => app.Trip.createAccount(app.Trip.hideModal));
